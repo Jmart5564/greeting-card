37424a9a883af9695176ed62d88c1055fe6b737f
Jmart5564 <Jmart5564@gmail.com>
2022-06-02 18:06:53 -0700

    Saving changes for the day

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 // grab DOM elements
 
+
 // set event listeners
+exportButton.addEventListener('click', async () => {
+    const dataUrl = await domtoimage.toPng(card);
+    const link = document.createElement('a');
+    link.download = nameInput.value + '.png';
+    link.href = dataUrl;
+    link.click();
+  });
     // get info from user input
     // use user input to update state
     // update DOM to reflect the new state
