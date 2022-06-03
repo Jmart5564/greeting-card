16af8548a57585542e18f3ba4b9952445022de75
Jmart5564 <Jmart5564@gmail.com>
2022-06-03 12:19:48 -0700

    name input cde validation-confirm it works

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 // grab DOM elements
+const nameInput = document.getElementById('name-input');
+const nameOutput = document.getElementById('name-output');
 
 
 // set event listeners
+nameInput.addEventListener('input', () => {
+    nameOutput.textContent = nameInput.value;
+});
+
+
+// Export button function
 exportButton.addEventListener('click', async () => {
     const dataUrl = await domtoimage.toPng(card);
     const link = document.createElement('a');
